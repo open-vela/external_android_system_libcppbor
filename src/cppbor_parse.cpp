29edb644bd58129312ab3269f6b0900ee1f3162c
openvela-robot <openvela-robot@xiaomi.com>
2021-04-01 19:10:25 +0800

    Reject reserved values and indefinite lengths am: 42a7aa8fbf am: f816fae5dd am: 57bba0cf18 am: c553ce5ef8
    Original change: https://android-review.googlesource.com/c/platform/external/libcppbor/+/1658064
    
    Change-Id: Ida208ab33fa846ac3028c1770732e38b5eeac03c

--- src/cppbor_parse.cpp
+++ src/cppbor_parse.cpp
@@ -275,9 +275,6 @@ std::tuple<const uint8_t*, ParseClient*> parseRecursively(const uint8_t* begin, 
                     return handleBool(addlData, begin, pos, parseClient);
                 case NULL_V:
                     return handleNull(begin, pos, parseClient);
-                default:
-                    parseClient->error(begin, "Unsupported floating-point or simple value.");
-                    return {begin, nullptr};
             }
     }
     CHECK(false);  // Impossible to get here.
